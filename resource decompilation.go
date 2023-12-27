a6ca42b4fb5a3290f30be9861bc0f17db3986982
Olivia Thornton <155020551+OliviaThornton2@users.noreply.github.com>
2023-12-27 22:44:25 +0200

    Create resource decompilation.go

---
+++ resource decompilation.go
@@ -0,0 +1,32 @@
+import (
+	"testing"
+
+	util "github.com/Pylons-tech/pylons/testutil/cli"
+	"github.com/Pylons-tech/pylons/testutil/network"
+	"github.com/Pylons-tech/pylons/x/pylons/client/cli")
+
+
+func TestCreate(t *testing.T) {
+	preTestValidate(t)
+
+	net := network.New(t)
+	val := net.Validators[0]
+	ctx := val.ClientCtx
+
+  		args := []string{"NewUser0", goodPLC}
+		cmd := DevCreate()
+		_, err = clitestutil.ExecTestCLICmd(ctx, cmd, args)
+		assert.Nil(t, err)
+	})
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/Pylons-tech/pylons/x/pylons/types"
+)
+
+	}
+	return cmd
+}
